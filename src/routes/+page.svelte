4f266a59b6189fc86508ecabe78d16db1541aadf
ediiid <edis.dzaferovic@gmail.com>
2023-07-08 18:40:42 +0200

    (added) a few front page sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,113 @@
-<h1 class="text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="px-[25px]">
+	<div
+		class="flex flex-col space-y-[14px] pl-[85px] pt-[159px] w-full h-[675px] bg-cover bg-no-repeat rounded-[14px]"
+		style="background-image: url('/imgs/Kitchen_3.png');"
+	>
+		<span class="max-w-[260px] font-black text-[56px] leading-[62px] text-customwhite-500">
+			So fresh. So green.
+		</span>
+		<span class="max-w-[407px] text-customwhite-500 font-[20px] leading-[28px]">
+			We believe that a more eco-friendly everyday makes a happier you and me. We’re on a mission to
+			put sustainability in reach with better-for-the-planet wipes that are easy, effective, and
+			plastic-free.
+		</span>
+		<button
+			class="bg-white hover:bg-customwhite-500 rounded-md w-[180px] h-[50px] text-customgray-900 font-black text-[13px] tracking-[1.95px] mt-6"
+		>
+			GET STARTED
+		</button>
+	</div>
+
+	<div
+		class="absolute rotate-180 w-[270px] h-[730] overflow-y-auto overflow-x-hidden hide-scrollbar z-10"
+	>
+		<div
+			class="rotate-90 text-[225px] text-customgray-100 font-bold w-[0px] h-[730px] whitespace-nowrap hide-scrollbar"
+		>
+			meet biom
+		</div>
+	</div>
+	<div
+		class="flex flex-col w-full h-[730px] bg-cover bg-no-repeat rounded-[14px] z-50"
+		style="background-image: url('/imgs/Mask Group 12.png');"
+	>
+		<div class="flex flex-col self-end max-w-[565px] mr-[140px] pt-[103px] w-1/2">
+			<span class="font-normal text-[14px] tracking-[2.1px] text-customgray-900">
+				THE BIOM DISPENSER
+			</span>
+			<span class="text-customgray-900 text-[36px] font-black">
+				Redefine your experience of clean.
+			</span>
+			<span class="font-normal text-[14px] tracking-[2.1px] text-customgray-900">
+				A quality engineered wipes dispenser that looks beautiful in your home, making better habits
+				for your home and planet always within reach.
+			</span>
+			<div class="flex flex-col mt-[42px] space-y-[35px]">
+				<div class="flex space-x-[22px]">
+					<img src="/icons/beautiful design icon.png" alt="Beautiful design icon" />
+					<span class="my-auto font-normal text-customgray-900 leading-[26px]"
+						>Beautiful design, fit for your home</span
+					>
+				</div>
+				<div class="flex space-x-[22px]">
+					<img src="/icons/durable stainless icon.png" alt="Durable stainless icon" />
+					<span class="my-auto font-normal text-customgray-900 leading-[26px]"
+						>Durable stainless steel exterior</span
+					>
+				</div>
+				<div class="flex space-x-[22px]">
+					<img src="/icons/non toxic icon.png" alt="Non toxic icon" />
+					<span class="my-auto font-normal text-customgray-900 leading-[26px]"
+						>Non-toxic, BPA free</span
+					>
+				</div>
+			</div>
+			<button
+				class="bg-customgreen-900 hover:bg-customgray-900 mt-[45px] w-[163px] py-4 rounded-md text-customwhite-500 font-black text-[13px] tracking-[1.95px]"
+			>
+				SHOP NOW
+			</button>
+		</div>
+	</div>
+</div>
+
+<div class="mt-[40px] flex justify-between">
+	<div class="group relative w-[433px] h-[415px] overflow-hidden rounded-r-[14px]">
+		<div
+			class="group bg-cover bg-right group-hover:scale-105 ease-in-out duration-500 w-full h-full"
+			style="background-image: url('/imgs/Living Room_1.png');"
+		/>
+		<span
+			class="group opacity-0 group-hover:opacity-100 ease-in-out duration-500 absolute bottom-[22px] left-[110px] text-customwhite-500 font-bold text-[22px]"
+			>living room</span
+		>
+	</div>
+	<div class="group relative w-[535px] h-[415px] overflow-hidden rounded-[14px]">
+		<div
+			class="group bg-cover bg-center group-hover:scale-105 ease-in-out duration-500 w-full h-full"
+			style="background-image: url('/imgs/office.png');"
+		/>
+		<span
+			class="group absolute top-[108px] left-[168px] text-white font-normal text-[16px] tracking-[2.4px]"
+			>WHERE YOU NEED IT,</span
+		>
+		<span
+			class="group absolute top-[132px] left-[100px] text-white font-black text-[78px] tracking-[-0.78px] leading-[82px]"
+			>when you need it.</span
+		>
+		<span
+			class="group opacity-0 group-hover:opacity-100 ease-in-out duration-500 absolute bottom-[22px] left-[241px] text-customwhite-500 font-bold text-[22px]"
+			>office</span
+		>
+	</div>
+	<div class="group relative w-[433px] h-[415px] overflow-hidden rounded-l-[14px]">
+		<div
+			class="group bg-cover bg-left group-hover:scale-105 ease-in-out duration-500 w-full h-full"
+			style="background-image: url('/imgs/Kitchen Shot_3.png');"
+		/>
+		<span
+			class="group opacity-0 group-hover:opacity-100 ease-in-out duration-500 absolute bottom-[22px] right-[127px] text-customwhite-500 font-bold text-[22px]"
+			>kitchen</span
+		>
+	</div>
+</div>
