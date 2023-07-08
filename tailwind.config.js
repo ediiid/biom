baec39a37c57c1c7833e20e6750516615cad3028
ediiid <edis.dzaferovic@gmail.com>
2023-07-08 14:47:31 +0200

    (added) custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+      colors: {
+        'customwhite': {
+          500: '#FFFAFA'
+        },
+        'customgray': {
+          100: '#F2F2F2',
+          900: '#002D33'
+        },
+        'customorange': {
+          500: '#DDA74A'
+        },
+        'customgreen': {
+          100: '#DAE5E5',
+          900: '#004A4C'
+        }
+      }
+    }
 	},
 	plugins: []
 };
