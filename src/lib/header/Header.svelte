0af449aa398f7547cb27a67bbcfc655229d43887
ediiid <edis.dzaferovic@gmail.com>
2023-07-09 17:43:19 +0200

    (fixed) header bg opacity

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -2,21 +2,23 @@
 	export let headerClass = '';
 </script>
 
-<header class="w-full sticky z-[100] top-0 backdrop-blur-[10px]">
+<header class="w-full sticky z-[100] top-0 backdrop-blur-[10px] bg-white bg-opacity-25">
 	<div class="flex justify-between {headerClass} px-[50px] py-[23px] align-middle">
 		<div
 			class="flex justify-start space-x-[22px] align-middle my-auto text-black text-[15px] font-bold"
 		>
-			<span> SHOP </span>
-			<span class="whitespace-nowrap"> WHY BIOM </span>
-			<span> SCENTS </span>
+			<a href="/"> SHOP </a>
+			<a href="/" class="whitespace-nowrap"> WHY BIOM </a>
+			<a href="/"> SCENTS </a>
 		</div>
-		<img src="/imgs/logo@2x.png" alt="BIOM logo" class="max-w-[92px] h-auto" />
+		<a href="/">
+			<img src="/imgs/logo@2x.png" alt="BIOM logo" class="max-w-[92px] h-auto" />
+		</a>
 		<div
 			class="flex justify-end space-x-[22px] align-middle my-auto text-black text-[15px] font-bold"
 		>
-			<span class="whitespace-nowrap"> SIGN IN </span>
-			<span> CART </span>
+			<a href="/" class="whitespace-nowrap"> SIGN IN </a>
+			<a href="/"> CART </a>
 			<div class="relative">
 				<img src="/icons/cart.svg" alt="Cart icon" class="max-w-[26px] h-auto" />
 				<div
